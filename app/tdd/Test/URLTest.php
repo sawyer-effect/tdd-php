02bb430bf56fd496c6a8103be48cd01668914e54
Javier Cervantes <1.27201@gmail.com>
2015-08-25 12:15:39 -0500

    Adding more URL tests

--- app/tdd/Test/URLTest.php
+++ app/tdd/Test/URLTest.php
@@ -6,15 +6,30 @@ use tdd\URL;
 
 class URLTest extends \PHPUnit_Framework_TestCase
 {
-
-    public function testSluggifyReturnsSluggifiedString()
+    /**
+     * @param string $originalString String to be sluggified
+     * @param string $expectedResult What we expect our slug result to be
+     *
+     * @dataProvider providerTestSluggifyReturnsSluggifiedString
+     */
+    public function testSluggifyReturnsSluggifiedString($originalString, $expectedResult)
     {
-        $originalString = 'This string will be sluggified';
-        $expectedResult = 'this-string-will-be-sluggified';
-        $url            = new URL();
-        
+        $url = new URL();
+
         $result = $url->sluggify($originalString);
 
         $this->assertEquals($expectedResult, $result);
+    }
+
+    public function providerTestSluggifyReturnsSluggifiedString()
+    {
+        return array(
+            array('This string will be sluggified', 'this-string-will-be-sluggified'),
+            array('THIS STRING WILL BE SLUGGIFIED', 'this-string-will-be-sluggified'),
+            array('This1 string2 will3 be 44 sluggified10', 'this1-string2-will3-be-44-sluggified10'),
+            array('This! @string#$ %$will ()be "sluggified', 'this-string-will-be-sluggified'),
+            array("Tänk efter nu – förr'n vi föser dig bort", 'tank-efter-nu-forrn-vi-foser-dig-bort'),
+            array('', ''),
+        );
     }
 }
